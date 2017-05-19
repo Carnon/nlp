78530c5b1ebea2cebe94418131044761e3ebc724
Carnon <376362798@qq.com>
2017-05-19 17:33:58 +0800

    add tfidf

--- TextCluster/textcluster/textcluster.py
+++ TextCluster/textcluster/textcluster.py
@@ -6,6 +6,8 @@ from collections import Counter
 
 import jieba
 from gensim.models import doc2vec
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.cluster import KMeans
 
 class TextCluster(object):
@@ -22,6 +24,7 @@ class TextCluster(object):
 		globalArgs.add_argument('--corpus',type=str,default='isr_question.txt',help='the base corpus file such as isr_question.txt')
 
 		trainArgs = parse.add_argument_group("Train options")
+		trainArgs.add_argument('--textRepresent',type=str,default='doc2vec',help='the style of text represention')
 		trainArgs.add_argument('--numCluster',type=int,default=3,help='the number of cluster')
 		trainArgs.add_argument('--maxIter',type=int,default=30,help='the number of iter')
 		return parse.parse_args(args)
@@ -39,34 +42,33 @@ class TextCluster(object):
 
 	def mainTrain(self):
 		print('train model....')
-		questions = doc2vec.TaggedLineDocument(self.seg_file)
-		dv = doc2vec.Doc2Vec(questions,size=200,window=300,min_count=1,workers=4)
-		self.km = KMeans(n_clusters=self.arg.numCluster,max_iter=self.arg.maxIter)
-		self.km.fit([vec for vec in dv.docvecs])
-
-		print('ok')
-
-
-		#tf_vectorizer = CountVectorizer()
-		#tf_matrix = tf_vectorizer.fit_transform(self.sen_seg_list)
-		#tfidf_transformer = TfidfTransformer()
-		#tfidf_matrix = tfidf_transformer.fit_transform(tf_matrix)
-		#self.km = KMeans(n_clusters=self.arg.numCluster,max_iter=self.arg.maxIter)
-		#self.km.fit(tfidf_matrix)
+		
+		if self.arg.textRepresent.lower() == 'doc2vec':
+			questions = doc2vec.TaggedLineDocument(self.seg_file)
+			dv = doc2vec.Doc2Vec(questions,size=200,window=300,min_count=1,workers=4)
+			self.km = KMeans(n_clusters=self.arg.numCluster,max_iter=self.arg.maxIter)
+			self.km.fit([vec for vec in dv.docvecs])
+		else:
+			tf_vectorizer = CountVectorizer()
+			tf_matrix = tf_vectorizer.fit_transform(self.sen_seg_list)
+			tfidf_transformer = TfidfTransformer()
+			tfidf_matrix = tfidf_transformer.fit_transform(tf_matrix)
+			self.km = KMeans(n_clusters=self.arg.numCluster,max_iter=self.arg.maxIter)
+			self.km.fit(tfidf_matrix)
 
 	def readCorpus(self):
 		print('read corpus ....')
 		self.corpus_list = []
-		sen_seg_list = []
+		self.sen_seg_list = []
 		dirName = os.path.join(os.getcwd(),self.DATA_DIR,self.arg.corpus)
 		with codecs.open(dirName,encoding='utf-8') as fr:
 			self.corpus_list=fr.readlines()
 			for corpus in self.corpus_list:
-				sen_seg_list.append(" ".join(jieba.cut(corpus.strip())))
+				self.sen_seg_list.append(" ".join(jieba.cut(corpus.strip())))
 		fileName = 'sen_seg.txt'
 		self.seg_file = os.path.join(os.getcwd(),self.RESULT_DIR,fileName)
 		with codecs.open(self.seg_file,'w+',encoding='utf-8') as fw:
-			for sen in sen_seg_list:
+			for sen in self.sen_seg_list:
 				fw.writelines(sen+'\n')
 
 
